b55017d66f21808b08865154e162650afb8a8d43
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-04-27 21:33:36 +0600

    Handling POST Requests

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Welcome to Homepage");
+  if (req.method === "POST" && req.url === "/submit") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Data received ", data: body }));
+    });
+    ///res.writeHead(200, { "content-type": "text/plain" });
+    ///res.end("Welcome to Homepage");
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
-    res.end("Page not Found");
+    res.end("Route not Found");
+
+    ///res.writeHead(404, { "content-type": "text/plain" });
+    ///res.end("Page not Found");
   }
 });
 
