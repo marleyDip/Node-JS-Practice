79b3a7034f6bf2edc3f828a7f863943a630a220a
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-04-28 09:59:50 +0600

    Simple Routes

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
 const http = require("http");
 const url = require("url");
 
+// Route Handler
+
+const routes = {
+  "/": (req, res) => {
+    res.writeHead(200, { "content-type": "text-plain" });
+    res.end("Welcome to Homepage");
+  },
+  "/about": (req, res) => {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("This is About Page");
+  },
+  "./notfound": (req, res) => {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("Page not Found");
+  },
+};
+
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url.startsWith("/search")) {
-    const queryObject = url.parse(req.url, true).query;
+  const { pathname } = url.parse(req.url);
 
-    res.writeHead(200, {
-      "content-type": "application/json",
-      "custom-header": "Node JS Server",
-      "custom-tracking": "1234",
-    });
-
-    res.end(JSON.stringify({ message: "Query received", queryObject }));
+  if (routes[pathname]) {
+    routes[pathname](req, res);
   } else {
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.end("route not found");
+    routes["./notfound"](req, res);
   }
-
-  /* if (req.method === "POST" && req.url === "/submit") {
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "Data received ", data: body }));
-    });
-    ///res.writeHead(200, { "content-type": "text/plain" });
-    ///res.end("Welcome to Homepage");
-  } else {
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.end("Route not Found");
-
-    ///res.writeHead(404, { "content-type": "text/plain" });
-    ///res.end("Page not Found");
-  } */
 });
 
 const port = 3000;
